3ed8c99806261ffb196f4db8827b976aeed01060
Gautam Tirkha <gautam.tirkha@voxturrlabs.com>
2024-10-22 16:31:26 +0530

    feat: Added debugContainer and debugBorder

--- lib/src/magic_widget_extension.dart
+++ lib/src/magic_widget_extension.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 /// Extension on [Widget] to provide convenient methods for wrapping
-/// widgets with [Flexible] and [Expanded].
+/// widgets with [Flexible], [Expanded], [Center], and debug widgets.
 extension MagicWidgetExtension on Widget {
   /// Wraps the widget in a [Flexible] widget.
   ///
@@ -51,7 +50,7 @@ extension MagicWidgetExtension on Widget {
         child: this,
       );
 
-  /// Wraps the widget in an [Center] widget.
+  /// Wraps the widget in a [Center] widget.
   ///
   /// The [key] parameter is optional and can be used to provide a
   /// [Key] for the [Center] widget.
@@ -61,11 +60,63 @@ extension MagicWidgetExtension on Widget {
   /// Container().center();
   /// ```
   Widget center({
-    int flex = 1,
     Key? key,
   }) =>
       Center(
         key: key,
+        child: this,
+      );
+
+  /// Wraps the widget in a [Container] with a debug color.
+  ///
+  /// The [key] parameter is optional and can be used to provide a
+  /// [Key] for the [Container].
+  ///
+  /// The [color] parameter specifies the color of the debug container,
+  /// defaulting to red.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// Text('Debug').debugContainer();
+  /// ```
+  Widget debugContainer({
+    Key? key,
+    Color color = Colors.red,
+  }) =>
+      Container(
+        key: key,
+        color: color,
+        child: this,
+      );
+
+  /// Wraps the widget in a [Container] with a debug border.
+  ///
+  /// The [key] parameter is optional and can be used to provide a
+  /// [Key] for the [Container].
+  ///
+  /// The [color] parameter specifies the color of the border,
+  /// defaulting to red.
+  ///
+  /// The [thickness] parameter specifies the width of the border,
+  /// defaulting to `1`.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// Text('Debug Border').debugBorder();
+  /// ```
+  Widget debugBorder({
+    Key? key,
+    Color color = Colors.red,
+    double thickness = 1,
+  }) =>
+      Container(
+        key: key,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: color,
+            width: thickness,
+          ),
+        ),
         child: this,
       );
 }
